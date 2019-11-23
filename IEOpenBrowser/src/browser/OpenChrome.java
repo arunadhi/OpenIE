7229c14291bb69b1ac7dbe239b291144ac244900
arunadhi <pradeep.adhi@gmail.com>
2019-11-23 14:48:57 +0530

    new ie

---
+++ IEOpenBrowser/src/browser/OpenChrome.java
@@ -0,0 +1,25 @@
+package browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class OpenChrome {
+	
+	public static WebDriver driver;
+	
+	// open IE Browser
+	public static void openIE(){
+		
+		System.setProperty("webdriver.ie.driver", "E:\\Refresh Selenium\\Driver\\Updated Driver\\IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		driver.get("https://www.google.com");
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		openIE();
+	}
+
+}
